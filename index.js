c6a267a1ac137d8aab531915d42525d8381a8425
Djotajo <20823564+Djotajo@users.noreply.github.com>
2024-12-14 12:33:16 +0100

    Finish functional page with html and css

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const url = require("url");
 
 server = http
   .createServer((req, res) => {
     const baseUrl = `http://${req.headers.host}`;
     const currentUrl = new URL(req.url, baseUrl);
-    // res.writeHead(200, { "Content-Type": "text/html" });
-    // res.write(`<p>Full URL: ${currentUrl.toString()}</p>`);
-    // return res.end();
 
-    const file = path.join(__dirname, currentUrl.pathname.toString());
-    console.log(file);
-    fs.readFile(file, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end(`404 Error. File ovaj ${file} not found.`);
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+    const basePath =
+      currentUrl.pathname === "/"
+        ? "/index"
+        : path.basename(currentUrl.pathname, path.extname(currentUrl.pathname));
+
+    const htmlFile = path.join(__dirname, basePath + ".html");
+    const cssFile = path.join(__dirname, basePath + ".css");
+
+    const mimeTypes = {
+      ".html": "text/html",
+      ".css": "text/css",
+    };
+
+    const serveFile = (filePath, contentType) => {
+      fs.readFile(filePath, function (err, data) {
+        if (err) {
+          const errorFile = path.join(__dirname, "404.html");
+          fs.readFile(errorFile, function (error, errorData) {
+            if (error) {
+              res.writeHead(500, { "Content-Type": "text/html" });
+              return res.end(`Internal server error.`);
+            }
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write(errorData);
+            return res.end();
+          });
+          return;
+        }
+        res.writeHead(200, { "Content-Type": contentType });
+        res.write(data);
+        return res.end();
+      });
+    };
+
+    if (req.url.endsWith(".css")) {
+      serveFile(cssFile, mimeTypes[".css"]);
+    } else {
+      serveFile(htmlFile, mimeTypes[".html"]);
+    }
   })
-  .listen(8080);
-
-// res.writeHead(200, { 'Content-Type': 'application/json' });
-// res.end(JSON.stringify({
-//   data: 'Hello World!',
-// }));
-// });
-
-// server.listen(8000);
-//   const safePath = path.join(
-//     __dirname,
-//     currentUrl === "/" ? "/index.html" : req.url
-//   );
-// const file = path.join(
-//   __dirname,
-//   currentUrl === "/" ? "/index.html" : req.url
-// );
+  .listen(8080, () => {
+    console.log("Server running at http://localhost:8080/");
+  });
